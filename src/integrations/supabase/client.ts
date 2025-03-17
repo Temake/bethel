16f5e6b83bca6fb756a54f5d30f9c17681953dc8
Temake <teminioluwaopemipo@gmail.com>
2025-03-17 17:42:44 +0100

    Config final

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -24,6 +24,7 @@ export const supabase = createClient<Database>(
       persistSession: true,
       autoRefreshToken: true,
       detectSessionInUrl: true,
+      redirectTo: getRedirectURL(),
       flowType: 'pkce',
     }
   }
